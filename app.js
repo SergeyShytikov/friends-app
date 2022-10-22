02d4b9d75bf174ecb4309edc446717bfc64dcd35
Sergey Shytikov <sergeyshytikov@gmail.com>
2022-10-22 10:29:01 +0200

    few friends  are displayed

--- app.js
+++ app.js
@@ -1,46 +1,29 @@
 // API's
 //"https://reqres.in/api/users/"
 //"https://randomuser.me/api/?results=10"
-// async function main() {
-//   const fakeUsers = await fetchUsers(RANDOM_USERS_URL);
-//   renderUsers(fakeUsers);
-//   users = [...fakeUsers];
-// }
 
-const url = "https://randomuser.me/api/?results=10";
+const url =
+  "https://randomuser.me/api/?results=10&inc=gender,name,email,dob,phone,picture&seed=foobar";
 const getData = async () => {
   const request = await fetch(url);
   const json = await request.json();
   return json.results;
 };
-const friendsList = getData();
+const friendsList = await getData();
 let friends = [...friendsList];
-console.log(friends);
-
-// let result = "";
-// console.log(users);
-
-// avatar: "https://reqres.in/img/faces/1-image.jpg"
-// email: "george.bluth@reqres.in"
-// first_name: "George"
-// id: 1
-// last_name: "Bluth"
-
-// const usersList = document.createDocumentFragment();
-//     users.forEach((user) => {
-//       const markup = `
-//   <div class="person">
-//   <img src="user.avatar" alt="user photo"></img>
-//     <h2>
-//         ${user.first_name}
-//         ${user.last_name}
-//     </h2>
-//     <p class="email">${user.email}</p>
-//   </div>
-// `;
-//       usersList.innerHTML = markup;
-//     });
-// console.log(usersList);
-// document.body.append(usersList);
-// document.body.innerHTML =
-// });
+const root = document.querySelector("#root");
+let fragment = document.createDocumentFragment();
+friends.forEach((friend) => {
+  fragment += `
+  <div class="person">
+  <img src="${friend.picture.medium}" alt="${friend.name.first} photo"></img>
+    <h2>
+        ${friend.name.first}
+        ${friend.name.last}
+    </h2>
+    <p class="email">${friend.email}</p>
+  </div>
+`;
+});
+root.innerHTML = fragment;
+console.log(root);
